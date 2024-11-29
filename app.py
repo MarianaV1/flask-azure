4c4d54c907b7a468e2fd2c49a94477a083f7315a
MarianaV1 <marianamvh2002@gmail.com>
2024-11-29 16:57:24 -0600

    Update saludo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Ruta para el endpoint GET
 @app.route('/saludo', methods=['GET'])
 def saludo():
-    return jsonify({'mensaje': 'Hola Mundo'}), 200
+    return jsonify({'mensaje': 'Hola desde Azure con deploy automático !!'}), 200
 
 # Ejecución de la app
 if __name__ == '__main__':
